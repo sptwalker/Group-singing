107b13014f86ae4e2e0abc7e2581d8b42efd756d
ericliu <ericliu@youdoogo.com>
2026-04-17 20:08:32 +0800

    用docker compose部署到服务器上，使用https://sing.youdoogo.com访问

--- youdoo-sing/frontend/src/pages/Home.tsx
+++ youdoo-sing/frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = 'http://sing.youdoogo.com:8000'
 
 function Home() {
   const [health, setHealth] = useState<string>('checking...')
